c505cca69cd27347f04a7ce7493895bb49ab273b
Istaufiqu <taufiqmail01@gmail.com>
2025-01-12 20:57:29 +0700

    Update estimasi_mobil.py

--- estimasi_mobil.py
+++ estimasi_mobil.py
@@ -6,16 +6,20 @@ model = pickle.load(open('estimasi_mobil.sav', 'rb'))
 
 st.title('Estimasi Harga Mobil Bekas')
 
-# Mengubah tipe input menjadi bilangan bulat (integer)
-year = st.number_input('Input Tahun Mobil', min_value=1900, max_value=2025, step=1, format="%d")
-mileage = st.number_input('Input Km Mobil', min_value=0, step=1, format="%d")
-tax = st.number_input('Input Pajak Mobil', min_value=0, step=1, format="%d")
-mpg = st.number_input('Input Konsumsi BBM Mobil', min_value=0, step=1, format="%d")
-engineSize = st.number_input('Input Engine Size', min_value=0, step=1, format="%d")
+# Mengatur input kosong secara default
+year = st.number_input('Input Tahun Mobil', min_value=1900, max_value=2025, step=1, format="%d", value=0)
+mileage = st.number_input('Input Km Mobil', min_value=0, step=1, format="%d", value=0)
+tax = st.number_input('Input Pajak Mobil', min_value=0, step=1, format="%d", value=0)
+mpg = st.number_input('Input Konsumsi BBM Mobil', min_value=0, step=1, format="%d", value=0)
+engineSize = st.number_input('Input Engine Size', min_value=0, step=1, format="%d", value=0)
 
 predict = ''
 
+# Validasi input: jika ada input yang kosong (masih bernilai 0), tampilkan pesan peringatan
 if st.button('Estimasi Harga'):
-    predict = model.predict([[year, mileage, tax, mpg, engineSize]])
-    st.write('Estimasi harga mobil bekas dalam Pounds:', predict)
-    st.write('Estimasi harga mobil bekas dalam IDR (Juta):', predict * 19000)
+    if year == 0 or mileage == 0 or tax == 0 or mpg == 0 or engineSize == 0:
+        st.warning('Harap isi semua input terlebih dahulu!')
+    else:
+        predict = model.predict([[year, mileage, tax, mpg, engineSize]])
+        st.write('Estimasi harga mobil bekas dalam Pounds:', predict)
+        st.write('Estimasi harga mobil bekas dalam IDR (Juta):', predict * 19000)

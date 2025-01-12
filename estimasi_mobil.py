9cea71c2049994c48206ce28fd0416d977e916a1
Istaufiqu <taufiqmail01@gmail.com>
2025-01-12 20:54:33 +0700

    Update estimasi_mobil.py

--- estimasi_mobil.py
+++ estimasi_mobil.py
@@ -1,21 +1,21 @@
 import pickle
 import streamlit as st
 
+# Memuat model yang sudah disimpan
 model = pickle.load(open('estimasi_mobil.sav', 'rb'))
 
 st.title('Estimasi Harga Mobil Bekas')
 
-year = st.number_input('Input Tahun Mobil')
-mileage = st.number_input('Input Km Mobil')
-tax = st.number_input('Input Pajak Mobil')
-mpg = st.number_input('Input Konsumsi BBM Mobil')
-engineSize = st.number_input('Input Engine Size')
+# Mengubah tipe input menjadi bilangan bulat (integer)
+year = st.number_input('Input Tahun Mobil', min_value=1900, max_value=2025, step=1, format="%d")
+mileage = st.number_input('Input Km Mobil', min_value=0, step=1, format="%d")
+tax = st.number_input('Input Pajak Mobil', min_value=0, step=1, format="%d")
+mpg = st.number_input('Input Konsumsi BBM Mobil', min_value=0, step=1, format="%d")
+engineSize = st.number_input('Input Engine Size', min_value=0, step=1, format="%d")
 
 predict = ''
 
 if st.button('Estimasi Harga'):
-    predict = model.predict(
-        [[year, mileage, tax, mpg, engineSize]]
-    )
-    st.write ('Estimasi harga mobil bekas dalam Ponds : ', predict)
-    st.write ('Estimasi harga mobil bekas dalam IDR (Juta) :', predict*19000)
+    predict = model.predict([[year, mileage, tax, mpg, engineSize]])
+    st.write('Estimasi harga mobil bekas dalam Pounds:', predict)
+    st.write('Estimasi harga mobil bekas dalam IDR (Juta):', predict * 19000)
